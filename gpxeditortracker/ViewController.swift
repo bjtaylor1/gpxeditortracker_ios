730209bfc6c32bcb4d540c58a3b0c5bcb4107328
Ben Taylor <bentaylortheonly@gmail.com>
2019-09-15 18:02:44 +0100

    Initial Commit

---
+++ gpxeditortracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gpxeditortracker
+//
+//  Created by Ben Taylor on 15/09/2019.
+//  Copyright © 2019 Ben Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
