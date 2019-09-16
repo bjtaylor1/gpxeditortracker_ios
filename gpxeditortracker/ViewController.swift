cb7a1cc4c28d2b9183da3edaf02f15b24a39fffa
Ben Taylor <bentaylortheonly@gmail.com>
2019-09-16 09:14:12 +0100

    handle viewDidBecomeActiveNotification

--- gpxeditortracker/ViewController.swift
+++ gpxeditortracker/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var lastReceivedLabel: UILabel!
+    @IBOutlet weak var lastUploadedLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(forName: UIApplication.didBecomeActiveNotification, object: nil, queue: nil)
+            {(notification) in self.handleNotification(notification: notification) }
+    }
+    
+    func handleNotification(notification:Notification) {
+        NSLog("didBecomeActiveNotification")
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        NSLog("viewDidAppear")
+        locationLabel.text = "the last location"
+        lastReceivedLabel.text = "the time it was received"
+        lastUploadedLabel.text = "the time it was uploaded"
+    }
 
 }
 
